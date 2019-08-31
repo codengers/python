665c9a34d217446340693fa5a9c261079a8d270c
Codengers <codengers@gmail.com>
2019-09-01 00:27:10 +0530

    string shifting - interview question

---
+++ string_shift.py
@@ -0,0 +1,17 @@
+"""
+string shift by 2 two steps
+Interview question
+provide string and integer times to shift
+"""
+
+
+def string_shift(word_str, bit):
+    shfited_str = ''
+    for s in word_str:
+        shfited_str += chr(ord(s) + int(bit))
+    return shfited_str
+
+
+input_str = input("Please enter string: ")
+
+print(input_str, string_shift(input_str, 2), end="")
